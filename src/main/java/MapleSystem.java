9ff80c4756d7b37d9900cbdb9337706ce92a5193
Cody Doucette <doucette@bu.edu>
2015-02-28 15:10:30 -0500

    Change style to be consistent

--- src/main/java/MapleSystem.java
+++ src/main/java/MapleSystem.java
@@ -1,26 +1,28 @@
 /**
  * Created by zhushigang on 2/21/15.
  */
+
 package org.maple.core;
-public class MapleSystem{
-    Controller C;
+
+public class MapleSystem {
+  Controller C;
   MapleFunction userFunction;
 
-    public void init(Controller c){
-        this.C = c;
+  public void init(Controller c) {
+    this.C = c;
 
-        // TODO: pass userFunction as argument into init, rather than
-        // hard-coding to be SampleMapleFunction.
-        this.userFunction = new SampleMapleFunction(); 
-    }
+    // TODO: pass userFunction as argument into init, rather than
+    // hard-coding to be SampleMapleFunction.
+    this.userFunction = new SampleMapleFunction(); 
+  }
 
-    public void handlePacket(byte[] data){
-        System.out.println("Maple received the packet. Thanks!");
+  public void handlePacket(byte[] data) {
+    System.out.println("Maple received the packet. Thanks!!!!!!!");
 
-        int out = userFunction.onPacket(data.length);
-        System.out.println("User's MapleFunction returned: " + out);
+    int out = userFunction.onPacket(data.length);
+    System.out.println("User's MapleFunction returned: " + out);
 
-        if (C != null)
-            C.sendPacket(data);
-    }
+    if (C != null)
+      C.sendPacket(data);
+  }
 }
