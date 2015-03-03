a275d02480b19b1efd1ef8d89f5c67f407245754
zhushigang <zhushigang@gmail.com>
2015-03-03 16:48:04 -0500

    Partial implementation of Learning Switch algorithm

--- src/main/java/LearningSwitch.java
+++ src/main/java/LearningSwitch.java
@@ -4,15 +4,30 @@
 
 package org.maple.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LearningSwitch extends MapleFunction {
+
+  private Map<String, Integer> MacTable;
 
   public LearningSwitch() {
     //TODO: initiate the Hashmap for port-address
+    MacTable = new HashMap<String, Integer>();
   }
 
   @Override
   public int onPacket(Packet p, int switchID, int portID) {
     //TODO: implement the learning algorithm using Hashmap
+    String src_mac = "1"; //TODO: replace with source mac from p
+    MacTable.put(src_mac,portID);
+    String dst_mac = "2"; // TODO; replace with destination mac from p
+    if(MacTable.containsKey(dst_mac)==false) {
+      //TODO: return broadcast
+    }else {
+      int dst_portID = MacTable.get(dst_mac);
+      //TODO: forward to dst_portID
+    }
     return 0;
   }
 }
--- src/main/java/MapleSystem.java
+++ src/main/java/MapleSystem.java
@@ -2,7 +2,7 @@
  * Created by zhushigang on 2/21/15.
  */
 
-package org.maple.core;
+  package org.maple.core;
 
 public class MapleSystem {
 
