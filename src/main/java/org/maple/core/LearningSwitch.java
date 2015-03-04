9aa2eaa8e4a338263d4cc060a25ac7d1521c9e26
zhushigang <zhushigang@gmail.com>
2015-03-04 13:40:19 -0500

    Add LearningSwitch Impl

--- src/main/java/org/maple/core/LearningSwitch.java
+++ src/main/java/org/maple/core/LearningSwitch.java
@@ -4,15 +4,26 @@
 
 package org.maple.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LearningSwitch extends MapleFunction {
+  private Map<Long, Integer> MacTable;
 
   public LearningSwitch() {
-    //TODO: initiate the Hashmap for port-address
+    MacTable = new HashMap<Long, Integer>();
   }
 
   @Override
   public int onPacket(Packet p) {
-    //TODO: implement the learning algorithm using Hashmap
-    return 0;
+    long src_mac = p.ethSrc();
+    MacTable.put(src_mac,p.ingressPort());
+    long dst_mac = p.ethDst();
+    if(MacTable.containsKey(dst_mac)==false) {
+      return Integer.MAX_VALUE;
+    }else {
+      int dst_portID = MacTable.get(dst_mac);
+      return dst_portID;
+    }
   }
 }
