f6d439203a17a4e725320e7cf0d26f552308f9e9
Cody Doucette <doucette@bu.edu>
2015-03-15 11:43:37 -0400

    Add environment and switch ID

--- src/main/java/org/maple/core/MapleSystem.java
+++ src/main/java/org/maple/core/MapleSystem.java
@@ -6,11 +6,15 @@ package org.maple.core;
 
 public class MapleSystem {
 
-  private Controller C;
+  private Controller controller;
   private MapleFunction userFunction;
 
   public MapleSystem(Controller c) {
-    this.C = c;
+    if (c == null)
+      throw new IllegalArgumentException("controller passed to " +
+                                         "MapleSystem must be non-null");
+
+    this.controller = c;
 
     // TODO: pass userFunction as an argument into init,
     // rather than hard-coding it.
@@ -18,20 +22,20 @@ public class MapleSystem {
     this.userFunction = new LearningSwitch();
   }
 
-  public void handlePacket(byte[] data, int ingressPort) {
-    System.out.println("Maple received a packet with ingressPort: " + 
-                       ingressPort + " and frame len: " + data.length);
+  public void handlePacket(Object[] env, byte[] data,
+                           int inSwitch, int inPort) {
+    System.out.println("Maple received a packet with inPort: " +
+                       inPort + " and frame len: " + data.length);
 
-    Ethernet frame = new Ethernet();;
+    Ethernet frame = new Ethernet();
     frame.deserialize(data, 0, data.length);
     System.out.println("handlePacket.frame: " + frame);
 
-    Packet p = new Packet(frame, ingressPort); 
+    Packet p = new Packet(frame, inPort);
 
     int out = userFunction.onPacket(p);
     System.out.println("User's MapleFunction returned: " + out);
 
-    if (C != null)
-      C.sendPacket(data, out);
+    controller.sendPacket(env, data, out);
   }
 }
