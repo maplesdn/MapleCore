241958f5d193da57e1f276d6f0e70365d48e353c
Andreas Voellmy <andreas.voellmy@gmail.com>
2015-04-11 18:20:25 -0400

    Add tests for evaluate.

--- src/main/java/org/maple/core/TraceTree.java
+++ src/main/java/org/maple/core/TraceTree.java
@@ -9,5 +9,9 @@ public class TraceTree {
   public void augment(List<TraceItem> trace, int... ports) {
 
   }
+
+  public int[] evaluate(int inPort, Ethernet frame) {
+    return null;
+  }
   
 }
--- src/test/java/org/maple/core/TraceTreeTest.java
+++ src/test/java/org/maple/core/TraceTreeTest.java
@@ -11,47 +11,79 @@ import java.util.LinkedList;
 
 public class TraceTreeTest {
 
-    @Test
-    public void testAugment1() {
+  int[] data = {0,0,0,0,0,2,0,0,0,0,0,4,8,0,69,0,0,84,73,114,0,0,64,1,
+                29,50,10,0,0,4,10,0,0,2,0,0,241,88,120,214,0,1,250,39,16,
+                85,0,0,0,0,192,127,12,0,0,0,0,0,16,17,18,19,20,21,22,23,
+                24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,
+                43,44,45,46,47,48,49,50,51,52,53,54,55};
 
-      TraceTree tree = new TraceTree();
+  @Test
+  public void testAugment1() {
 
-      assertTrue(tree.root == null);
+    TraceTree tree = new TraceTree();
 
-      LinkedList<TraceItem> emptyTrace = new LinkedList<TraceItem>();
-      int[] outcome = {1,2,3};
-      tree.augment(emptyTrace, outcome);
+    assertTrue(tree.root == null);
+    
+    LinkedList<TraceItem> emptyTrace = new LinkedList<TraceItem>();
+    int[] outcome = {1,2,3};
+    tree.augment(emptyTrace, outcome);
 
-      assertTrue(tree.root != null);
-      assertTrue(tree.root instanceof L);
-      assertEquals(outcome, ((L) tree.root).outcome);
-    }
+    assertTrue(tree.root != null);
+    assertTrue(tree.root instanceof L);
+    assertEquals(outcome, ((L) tree.root).outcome);
+
+    byte[] frameBytes = makeFrameBytes(data);
+    Ethernet frame = new Ethernet();
+    frame.deserialize(frameBytes, 0, frameBytes.length);
+    assertNull(tree.evaluate(10, frame));
+    
+  }
 
   
-    @Test
-    public void testAugment2() {
+  @Test
+  public void testAugment2() {
 
-      TraceTree tree = new TraceTree();
+    TraceTree tree = new TraceTree();
 
-      assertTrue(tree.root == null);
+    assertTrue(tree.root == null);
 
-      LinkedList<TraceItem> trace = new LinkedList<TraceItem>();
-      trace.add(TraceItem.ethSrcItem(1234L));
-      int[] outcome = {1,2,3};
-      tree.augment(trace, outcome);
+    int PORT = 10;
+    
+    LinkedList<TraceItem> trace = new LinkedList<TraceItem>();
+    trace.add(TraceItem.inPortItem(PORT));
+    int[] outcome = {1,2,3};
+    tree.augment(trace, outcome);
 
-      assertTrue(tree.root != null);
-      assertTrue(tree.root instanceof V);
+    assertTrue(tree.root != null);
+    assertTrue(tree.root instanceof V);
 
-      V node = (V) tree.root;
+    V node = (V) tree.root;
 
-      assertTrue(null == node.getChild(9000L));
-      assertTrue(null != node.getChild(1234L));
+    assertTrue(null == node.getChild(PORT+1));
+    assertTrue(null != node.getChild(PORT));
 
-      assertTrue(node.getChild(1234L) instanceof L);
-      L leaf = (L) node.getChild(1234L);
-      assertEquals(outcome, leaf.outcome);
+    assertTrue(node.getChild(PORT) instanceof L);
+    L leaf = (L) node.getChild(PORT);
+    assertEquals(outcome, leaf.outcome);
 
+    byte[] frameBytes = makeFrameBytes(data);
+    Ethernet frame = new Ethernet();
+    frame.deserialize(frameBytes, 0, frameBytes.length);
+    assertNull(tree.evaluate(PORT + 1, frame));    
+    assertNotNull(tree.evaluate(PORT, frame));
+    assertEquals(outcome, tree.evaluate(PORT, frame));
+    
+  }
+
+
+  
+  // Java does not support byte literals; therefore, we need to convert
+  // an array of ints to array of bytes.
+  byte[] makeFrameBytes(int[] data) {
+    byte[] frame = new byte[data.length];
+    for (int i = 0; i < frame.length; i++) {
+      frame[i] = (byte) data[i];
     }
-
+    return frame;
+  }
 }
