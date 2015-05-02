c884892da81584dd5f75e30e80519a3e8f1bbf49
Shigang Zhu <zhushigang@gmail.com>
2015-05-01 23:22:02 -0400

    Added implementation for calculating rules difference

---
+++ src/test/java/org/maple/core/RuleDiffTest.java
@@ -0,0 +1,66 @@
+package org.maple.core;
+
+/**
+ * Created by zhushigang on 2015/5/1.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.LinkedList;
+import java.util.HashSet;
+
+public class RuleDiffTest {
+
+    @Test
+    public void TestEquals1() {
+        Rule rule1 = new Rule(1,Match.matchAny(),Action.Drop());
+        Rule rule2 = new Rule(1,Match.matchAny(),Action.Drop());
+        assertTrue(rule1.equals(rule2));
+
+        Rule rule3 = new Rule(1,Match.matchAny(),Action.Punt());
+        assertTrue(!rule1.equals(rule3));
+
+        HashSet<TraceItem> fieldValues = new HashSet<TraceItem>();
+        fieldValues.add(TraceItem.inPort(10));
+        Match match1 = new Match(fieldValues);
+
+        Rule rule4 = new Rule(1,match1,Action.Drop());
+        assertTrue(!rule1.equals(rule4));
+    }
+
+    public void TestDiff1() {
+        LinkedList<Rule> oldRules = new LinkedList<Rule>();
+
+        Rule rule1 = new Rule(1,Match.matchAny(),Action.Drop());
+        oldRules.add(rule1);
+        Rule rule2 = new Rule(1,Match.matchAny(),Action.ToPorts(1));
+        oldRules.add(rule2);
+
+        LinkedList<Rule> newRules = new LinkedList<Rule>();
+        Rule rule3 = new Rule(1,Match.matchAny(),Action.Punt());
+        newRules.add(rule3);
+        HashSet<TraceItem> fieldValues = new HashSet<TraceItem>();
+        fieldValues.add(TraceItem.inPort(10));
+        Match match1 = new Match(fieldValues);
+        Rule rule4 = new Rule(1,match1,Action.Drop());
+        newRules.add(rule4);
+
+        Diff expectedDiff = new Diff(oldRules,newRules);
+
+        Controller c = new Controller() {
+            public void sendPacket(byte[] data, int inSwitch, int inPort, int... ports) {
+                System.out.println("In sendPacket");
+            }
+            public void installRules(LinkedList<Rule> rules, int... outSwitches) {
+                System.out.println("In installRules");
+            }
+        };
+
+        MapleSystem mapleSystem = new MapleSystem(c);
+
+        Diff diff = mapleSystem.diff(oldRules,newRules);
+
+        assertTrue(expectedDiff.added.equals(diff.added));
+        assertTrue(expectedDiff.removed.equals(diff.removed));
+    }
+}
