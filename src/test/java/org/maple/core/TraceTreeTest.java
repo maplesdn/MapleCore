bcdd0656c40115391a57f72941d4bd084827fad8
zhushigang <zhushigang@gmail.com>
2015-05-03 10:38:01 -0400

    Revert "test non-empty TraceTree augmented with non-empty trace"
    This reverts commit 5c83f077c94191c40b85dcc59f97070155f68788.

--- src/test/java/org/maple/core/TraceTreeTest.java
+++ src/test/java/org/maple/core/TraceTreeTest.java
@@ -10,7 +10,6 @@ import static org.junit.Assert.*;
 import java.util.LinkedList;
 import java.util.HashSet;
 
-
 public class TraceTreeTest {
 
   int[] data = {0,0,0,0,0,2,0,0,0,0,0,4,8,0,69,0,0,84,73,114,0,0,64,1,
@@ -20,54 +19,49 @@ public class TraceTreeTest {
                 43,44,45,46,47,48,49,50,51,52,53,54,55};
   int PORT = 10;
 
-
   @Test
-  // empty tree augmented by empty trace with outcome
-  public void testAugment1() {    
+  public void testAugment1() {
 
     TraceTree tree = new TraceTree();
 
-    assertNull(tree.root);
+    assertTrue(tree.root == null);
     
-    LinkedList<TraceItem> emptyTrace = new LinkedList<TraceItem>();  // import of augment
-    int[] outcome = {1,2,3};   // user function return
-    tree.augment(emptyTrace, outcome);  
+    LinkedList<TraceItem> emptyTrace = new LinkedList<TraceItem>();
+    int[] outcome = {1,2,3};
+    tree.augment(emptyTrace, outcome);
 
-    // UnitTest will stop if any assert.. statement fails
-    assertNotNull(tree.root);
-    assertTrue(tree.root instanceof L); // L is leaf 
-    assertEquals(outcome, ((L) tree.root).outcome);  // cast tree.root to L leaf
-
+    assertTrue(tree.root != null);
+    assertTrue(tree.root instanceof L);
+    assertEquals(outcome, ((L) tree.root).outcome);
 
     byte[] frameBytes = makeFrameBytes(data);
-    Ethernet frame = new Ethernet();  // create import
+    Ethernet frame = new Ethernet();
     frame.deserialize(frameBytes, 0, frameBytes.length);
-    assertNotNull(tree.evaluate(PORT, frame));  // tree.evaluate: find all the L fulfill the requirements PORT and frame 
+    assertNotNull(tree.evaluate(PORT, frame));
     assertEquals(outcome, tree.evaluate(PORT, frame));
   }
 
   
   @Test
-  // empty tree augmented by non-empty trace(one inPort) with outcome
   public void testAugment2() {
 
     TraceTree tree = new TraceTree();
 
-    assertNull(tree.root);
+    assertTrue(tree.root == null);
 
     
     LinkedList<TraceItem> trace = new LinkedList<TraceItem>();
-    trace.add(TraceItem.inPort(PORT));  // trace's field inPort is PORT
+    trace.add(TraceItem.inPort(PORT));
     int[] outcome = {1,2,3};
     tree.augment(trace, outcome);
 
-    assertNotNull(tree.root);
-    assertTrue(tree.root instanceof V); // whether tree.root is V
+    assertTrue(tree.root != null);
+    assertTrue(tree.root instanceof V);
 
     V node = (V) tree.root;
 
-    assertNull(node.getChild(PORT+1));  // should return the child fulfill requirement
-    assertNotNull(node.getChild(PORT));
+    assertTrue(null == node.getChild(PORT+1));
+    assertTrue(null != node.getChild(PORT));
     assertTrue(node.getChild(PORT) instanceof L);
     L leaf = (L) node.getChild(PORT);
     assertEquals(outcome, leaf.outcome);
@@ -81,62 +75,6 @@ public class TraceTreeTest {
     
   }
 
-
-  @Test
-  // empty tree augmented by non-empty trace(one inPort and one ethDst) with outcome
-  // non-empty tree augmented by non-empty trace 
-  public void testAugment3() {
-    TraceTree tree = new TraceTree();
-
-    assertNull(tree.root);
-
-    LinkedList<TraceItem> trace = new LinkedList<TraceItem>();
-
-    trace.add(TraceItem.inPort(1));
-    trace.add(TraceItem.ethDst(0x02));
-    int[] outcome={2};
-
-    tree.augment(trace, outcome);
-
-    assertNotNull(tree.root);
-    V node = (V) tree.root;
-    V node2 = node.getChild(1);
-    assertNotNull(node2);
-    assertTrue(node2 instanceof V);
-    assertEquals(node2.field, trace.get(1).field);
-    assertNull(node2.getChild(0x03));
-    assertTrue(node2.getChild(0x02) instanceof L);
-    assertEquals(node2.getChild(0x02).outcome, outcome);
-
-    LinkedList<TraceItem> trace2 = new LinkedList<TraceItem>();
-
-    trace2.add(TraceItem.inPort(1));
-    trace2.add(TraceItem.ethDst(0x05));
-    int[] outcome2 = {5};
-
-    // non-empty tree
-    tree.augment(trace2, outcome2);
-
-    assertNotNull(tree.root);
-    V node = (V) tree.root;
-    V node 2 = node.getChild(1);
-
-    // make sure tree.root.getChild(1) keeps the same
-    assertNotNull(node2);
-    assertTrue(node2 instanceof V);
-    assertEquals(node2.field, trace.get(1).field);
-    assertNull(node2.getChild(0x03));
-    assertNotNull(node2.getChild(0x02));
-    assertTrue(node2.getChild(0x02) instanceof L);
-
-    // test tree.root.getChild(1).getChild(0x05)
-    assertNotNull(node2.getChild(0x05));
-    assertTrue(node2.getChild(0x05) instanceof L);
-    assertNotEquals(node2.getChild(0x05).outcome, outcome);
-    assertEquals(node2.getChild(0x05).outcome, outcome2);
-  }
-
-
   @Test
   public void testItemEq() {
     assertEquals(TraceItem.ethSrc(1),  TraceItem.ethSrc(1));
@@ -144,7 +82,6 @@ public class TraceTreeTest {
   }
 
   @Test
-  // Compile: each leaf has a rule (priority, match, actions)
   public void testCompile1() {
 
     TraceTree tree;
@@ -180,8 +117,6 @@ public class TraceTreeTest {
     assertEquals(rulesExpected, tree.compile());
   }
   
-
-
   // Java does not support byte literals; therefore, we need to convert
   // an array of ints to array of bytes.
   byte[] makeFrameBytes(int[] data) {
