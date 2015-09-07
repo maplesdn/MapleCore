6c06543eef0b6510781ca36438dc627baa6fb954
Junchang Wang <junchang.wang@gmail.com>
2015-09-07 11:07:20 +0800

    Enable generating rules from a TT with T nodes.

--- src/main/java/org/maple/core/TraceTree.java
+++ src/main/java/org/maple/core/TraceTree.java
@@ -3,6 +3,7 @@ package org.maple.core;
 import java.util.Iterator;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Set;
 
 public class TraceTree {
@@ -39,19 +40,27 @@ public class TraceTree {
     return rules;
   }
 
+  //TODO: This is a very naive version of generating rules from TT.
+  //      The first step towards optimizations is described in Maple paper Section 4.3.
   private void build(Node t,Match match) {
-    if (t instanceof L) {
+    if (t == null) {  // empty child of T nodes
+      Rule rule = new Rule(priority, match, Action.Punt());
+      rules.add(rule);
+      priority += 1;
+    }
+    else if (t instanceof L) {
       L leaf = (L) t;
       Action action = new ToPorts(leaf.outcome);
       Rule rule = new Rule(priority, match, action);
       rules.add(rule);
-    }
-    else if (t instanceof V){
+      priority += 1;
+    } 
+    else if (t instanceof V) {
       Set<Long> keys = ((V) t).subtree.keySet();
       Iterator<Long> iterator = keys.iterator();
       int i=0;
       while(iterator.hasNext()) {
-        TraceItem item = new TraceItem();
+        TraceItemV item = new TraceItemV();
         item.field = ((V) t).field;
         item.value = iterator.next();
         Match m = Match.matchAny();
@@ -61,9 +70,40 @@ public class TraceTree {
         m.add(item);
         build(((V) t).getChild(item.value),m);
       }
-    } else {
-      Rule rule = new Rule(priority, Match.matchAny(), Action.Punt());
+    } 
+    else if (t instanceof T){
+      Match m_neg = Match.matchAny(); 
+      for (TraceItem item2 : match.fieldValues) {
+        m_neg.add(item2);
+      }
+      build(((T) t).subtree[T.NEG_BRANCH],m_neg);
+      
+      TraceItemT item = new TraceItemT();
+      item.field = ((T) t).field;
+      item.value = ((T) t).value;
+      item.Tvalue = true;
+      Match m_barr = Match.matchAny(); 
+      for (TraceItem item2 : match.fieldValues) {
+        m_barr.add(item2);
+      }
+      m_barr.add(item);
+      Rule rule = new Rule(priority, m_barr, Action.Punt()); // barrier rule
       rules.add(rule);
+      
+      Match m_pos = Match.matchAny(); 
+      for (TraceItem item2 : match.fieldValues) {
+        m_pos.add(item2);
+      }
+      m_pos.add(item);
+      priority += 1;
+      build(((T) t).subtree[T.POS_BRANCH],m_pos);
+    }
+    else {
+      // We should never reach here.
+      //FIXME:TODO: I hate this. Replace println with a uniform LOG module.
+      System.out.println("Error in building rules: Unknown type of node in TT."); 
+      //Rule rule = new Rule(priority, Match.matchAny(), Action.Punt());
+      //rules.add(rule);
     }
   }
 
@@ -71,9 +111,15 @@ public class TraceTree {
     if (trace.isEmpty()) {
       return new L(ports);
     }
-    V treeRoot = new V();
-    treeRoot.augment(trace,ports);
-    return treeRoot;
+    if ( trace.get(0) instanceof TraceItemT) {
+      T treeRoot = new T();
+      treeRoot.augment(trace,ports);
+      return treeRoot;
+    }
+    else {
+      V treeRoot = new V();
+      treeRoot.augment(trace,ports);
+      return treeRoot;
+    }
   }
-  
 }
