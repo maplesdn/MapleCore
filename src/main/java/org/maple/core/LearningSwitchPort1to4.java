20dc96bdc9a8b7d48eb03e645f471c132dcb217d
Junchang Wang <junchang.wang@gmail.com>
2015-09-07 11:12:29 +0800

    Add a special AP application, LearningSwitchPort1to4.java, to test maplecore with T nodes.

---
+++ src/main/java/org/maple/core/LearningSwitchPort1to4.java
@@ -0,0 +1,42 @@
+/**
+ * Created by zhushigang on 2/28/15.
+ */
+
+package org.maple.core;
+
+import java.util.LinkedList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Example user defined algorithm policy which simple simulates the behaviour of a learing switch
+ */
+public class LearningSwitchPort1to4 extends MapleFunction {
+  private Map<Long, Integer> MacTable;
+
+  public LearningSwitchPort1to4() {
+    MacTable = new HashMap<Long, Integer>();
+  }
+
+  @Override
+  public Route onPacket(Packet p) {
+	  
+    boolean isPort1 = p.ingressPortIs(1);
+    if (isPort1) {
+      boolean isARP = p.ethTypeIs(Ethernet.TYPE_ARP);
+      if (isARP)
+        return Route.toPorts(4);
+    }
+    
+    // learningswitch
+    long src_mac = p.ethSrc();
+    MacTable.put(src_mac,p.ingressPort());
+    long dst_mac = p.ethDst();
+    if(MacTable.containsKey(dst_mac)==false) {
+      return Route.toPorts(ports());
+    } else {
+      int dst_portID = MacTable.get(dst_mac);
+      return Route.toPorts(dst_portID);
+    }
+  }
+}
