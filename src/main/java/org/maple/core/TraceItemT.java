0f59dc5a47ccdfacd04d08451de56cc0cdd2dc32
Junchang Wang <junchang.wang@gmail.com>
2015-09-07 09:19:47 +0800

    Create subclass TraceItemV to record T information in traces.

---
+++ src/main/java/org/maple/core/TraceItemT.java
@@ -0,0 +1,61 @@
+package org.maple.core;
+
+import org.maple.core.TraceItem.Field;
+
+public class TraceItemT extends TraceItem {
+	
+  @Override
+  public String toString() {
+    return "TraceItemT [field=" + field + ", value=" + value + ", Tvalue=" + Boolean.toString(Tvalue) + "] --> ";
+  }
+
+  @Override
+  public boolean equals(Object other) {
+    if (null == other) { return false; }
+    TraceItemT other2 = (TraceItemT) other;
+    return field==other2.field && value==other2.value && Tvalue==other2.Tvalue;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 5557;
+    int result = 1;
+    result = prime * result + (int) field.ordinal();
+    result = prime * result + (int) value;
+    if (Tvalue)
+      result += 1;
+    return result;
+  }
+
+  public static TraceItemT ethSrcIs(long addr, long exp) {
+    TraceItemT item = new TraceItemT();
+    item.field = TraceItemT.Field.ETH_SRC;
+    item.value = exp;
+    item.Tvalue = (addr == exp);
+    return item;
+  }
+
+  public static TraceItemT ethDstIs(long addr, long exp) {
+    TraceItemT item = new TraceItemT();
+    item.field = TraceItemT.Field.ETH_DST;
+    item.value = exp;
+    item.Tvalue = (addr == exp);
+    return item;
+  }
+
+  public static TraceItemT ethTypeIs(int typ, int exp) {
+    TraceItemT item = new TraceItemT();
+    item.field = TraceItemT.Field.ETH_TYPE;
+    item.value = exp;
+    item.Tvalue = (typ == exp);
+    return item;
+  }
+
+  public static TraceItemT inPortIs(int port, int exp) {
+    TraceItemT item = new TraceItemT();
+    item.field = TraceItemT.Field.IN_PORT;
+    item.value = exp;
+    item.Tvalue = (port == exp);
+    return item;
+  }
+}
