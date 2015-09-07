019838d0b16df69a759b0340d04682355a4451fd
Junchang Wang <junchang.wang@gmail.com>
2015-09-07 10:58:54 +0800

    TraceItem is now a parent class, which has two children, TraceItemV and TraceItemT.

--- src/main/java/org/maple/core/TraceItem.java
+++ src/main/java/org/maple/core/TraceItem.java
@@ -1,6 +1,6 @@
 package org.maple.core;
 
-public class TraceItem {
+public abstract class TraceItem {
 
   public enum Field {
     IN_PORT,
@@ -11,54 +11,10 @@ public class TraceItem {
   
   public Field field;
   public long value;
+  public boolean Tvalue;
 
-  @Override
-  public String toString() {
-    return "TraceItem [field=" + field + ", value=" + value + "]";
-  }
-
-  @Override
-  public boolean equals(Object other) {
-    if (null == other) { return false; }
-    TraceItem other2 = (TraceItem) other;
-    return field==other2.field && value==other2.value;
-  }
-
-  @Override
-  public int hashCode() {
-    final int prime = 5557;
-    int result = 1;
-    result = prime * result + (int) field.ordinal();
-    result = prime * result + (int) value;
-    return result;
-  }
-
-  public static TraceItem ethSrc(long addr) {
-    TraceItem item = new TraceItem();
-    item.field = TraceItem.Field.ETH_SRC;
-    item.value = addr;
-    return item;
-  }
-
-  public static TraceItem ethDst(long addr) {
-    TraceItem item = new TraceItem();
-    item.field = TraceItem.Field.ETH_DST;
-    item.value = addr;
-    return item;
-  }
-
-  public static TraceItem ethType(int typ) {
-    TraceItem item = new TraceItem();
-    item.field = TraceItem.Field.ETH_TYPE;
-    item.value = typ;
-    return item;
-  }
-
-  public static TraceItem inPort(int port) {
-    TraceItem item = new TraceItem();
-    item.field = TraceItem.Field.IN_PORT;
-    item.value = port;
-    return item;
-  }
+  public abstract String toString();
+  public abstract boolean equals(Object other);
+  public abstract int hashCode();
 
 }
